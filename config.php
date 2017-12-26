dd4212f2843c257cb0070ba7bf301adccfc31c4c
Raul R <raul.ramirez@garden.com.py>
2017-12-26 18:04:00 -0300

    Se agrega validacion extra en el config

--- config.php
+++ config.php
@@ -8,6 +8,12 @@
 // Always provide a TRAILING SLASH (/) AFTER A PATH
 $host = getHost();
 switch ($host) {
+    case 'www.garden.com.py':
+        define('URL', 'http://www.garden.com.py/infogarden2017/');
+        define('DB_USER', 'web');
+        define('DB_PASS', 'WebG@rdenMKT');
+        define('DB_NAME', 'infonews');
+        break;
     case 'garden.com.py':
         define('URL', 'http://www.garden.com.py/infogarden2017/');
         define('DB_USER', 'web');
